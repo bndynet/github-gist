f16ed5525485356c18d60b2332eff5092710de8b
Bendy Zhang <zb@bndy.net>
2018-12-30 12:32:53 +0800

    fix authorization uri error

--- src/config/app.prod.tsx
+++ src/config/app.prod.tsx
@@ -4,7 +4,7 @@ const config: Config = {
     clientId: 'eb6ab71a97ef2692d857',
     clientSecret: '014e9f84049964807d751c81658d133a1e37acd3',
     authorizationCallbackUri: 'https://gist.bndy.net/auth/callback',
-    authorizationUri: 'https://cloud.bndy.net/service-auth/authorize',
+    authorizationUri: 'https://cloud.bndy.net/service-oauth/authorize',
 };
 
 module.exports = config;
